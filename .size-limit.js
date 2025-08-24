4730042f4841b3065f29b2cc9774fd27b42db343
supahuman <successkoncepts@gmail.com>
2025-08-24 07:50:10 -0500

    fix: correct formatting in size-limit configuration

--- .size-limit.js
+++ .size-limit.js
@@ -2,6 +2,6 @@ module.exports = [
   {
     path: "dist/index.js",
     limit: "10 KB",
-    plugins: ['@size-limit/esbuild', '@size-limit/esbuild-why']
-  }
+    plugins: ["@size-limit/esbuild", "@size-limit/esbuild-why"],
+  },
 ];
